c6a00b92be021f0061cc914881b512c7db3da361
ANDREEA CEACA <CEACA.ANDREEA@YAHOO.COM>
2022-05-18 20:10:28 +0100

    Lab 17

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,59 +1,63 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
+    private WebDriver driver;
 
-
-    public void loginWithValidCredentialsTest(){
+    @Before
+    public void openDriver() {
         System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://testfasttrackit.info/selenium-test/");
-        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
-//        WebElement accountLink = driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label"));
-//        accountLink.click();
-        driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
+    }
+
+    @Test
+    public void loginWithValidCredentialsTest(){
+        driver.findElement(By.cssSelector(".skip-account .label")).click();
+        driver.findElement(By.cssSelector("a[title='Log In']")).click();
         driver.findElement(By.id("email")).sendKeys("cosmin@fasttrackit.org");
         driver.findElement(By.id("pass")).sendKeys("123456");
-        driver.findElement(By.cssSelector("#send2 > span > span")).click();
-        driver.close();
+        driver.findElement(By.id("send2")).click();
+        WebElement dashboardTextElement = driver.findElement(By.cssSelector(".page-title h1"));
+        String textFromElement = driver.findElement(By.cssSelector(".hello strong")).getText();
+        Assert.assertTrue(dashboardTextElement.isDisplayed());
+        Assert.assertEquals("Hello, Cosmin Fast!", textFromElement);
     }
 
+    @Test
     public void loginWithInvalidPasswordTest(){
-        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("http://testfasttrackit.info/selenium-test/");
-        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
-        driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
+        driver.findElement(By.cssSelector(".skip-account .label")).click();
+        driver.findElement(By.cssSelector("a[title='Log In']")).click();
         driver.findElement(By.id("email")).sendKeys("cosmin@fasttrackit.org");
         driver.findElement(By.id("pass")).sendKeys("121233456");
-        driver.findElement(By.cssSelector("#send2 > span > span")).click();
-        driver.close();
+        driver.findElement(By.id("send2")).click();
     }
 
+    @Test
     public void loginWithInvalidEmail(){
-        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("http://testfasttrackit.info/selenium-test/");
-        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
-        driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
+        driver.findElement(By.cssSelector(".skip-account .label")).click();
+        driver.findElement(By.cssSelector("a[title='Log In']")).click();
         driver.findElement(By.id("email")).sendKeys("cosminfasttrackit.org");
         driver.findElement(By.id("pass")).sendKeys("121233456");
-        driver.findElement(By.cssSelector("#send2 > span > span")).click();
-        driver.close();
+        driver.findElement(By.id("send2")).click();
     }
 
+    @Test
     public void loginWithoutCredentialsTest(){
-        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("http://testfasttrackit.info/selenium-test/");
-        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
-        driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
-        driver.findElement(By.cssSelector("#send2 > span > span")).click();
+        driver.findElement(By.cssSelector(".skip-account .label")).click();
+        driver.findElement(By.cssSelector("a[title='Log In']")).click();
+        driver.findElement(By.id("send2")).click();
+    }
+
+    @After
+    public void close() {
         driver.close();
     }
 }
--- src/test/java/RegisterTest.java
+++ src/test/java/RegisterTest.java
@@ -1,42 +1,49 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class RegisterTest {
+    private WebDriver driver;
 
+    @Before
+    public void openDriver() {
+        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://testfasttrackit.info/selenium-test/");
+    }
+
+    @Test
     public void validRegisterTest(){
-        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("http://testfasttrackit.info/selenium-test/");
-        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
-        driver.findElement(By.cssSelector("#header-account > div > ul > li:nth-child(5) > a")).click();
+        driver.findElement(By.cssSelector(".skip-account .label")).click();
+        driver.findElement(By.cssSelector("a[title='Register']")).click();
         driver.findElement(By.id("firstname")).sendKeys("And");
         driver.findElement(By.id("middlename")).sendKeys("blabla");
         driver.findElement(By.id("lastname")).sendKeys("Maaaaa");
         driver.findElement(By.id("email_address")).sendKeys("test@yahoo.com");
         driver.findElement(By.id("password")).sendKeys("pass");
         driver.findElement(By.id("confirmation")).sendKeys("pass");
-        driver.findElement(By.cssSelector("#form-validate > div.buttons-set > button > span > span")).click();
-        driver.close();
+        driver.findElement(By.cssSelector(".button.validation-passed")).click();
     }
 
+    @Test
     public void invalidRegisterTest(){
-        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("http://testfasttrackit.info/selenium-test/");
-        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
-        driver.findElement(By.cssSelector("#header-account > div > ul > li:nth-child(5) > a")).click();
+        driver.findElement(By.cssSelector(".skip-account .label")).click();
+        driver.findElement(By.cssSelector("a[title='Register']")).click();
         driver.findElement(By.id("firstname")).sendKeys("And");
         driver.findElement(By.id("middlename")).sendKeys("blabla");
         driver.findElement(By.id("lastname")).sendKeys("Maaaaa");
         driver.findElement(By.id("email_address")).sendKeys("test@yahoo.com");
         driver.findElement(By.id("password")).sendKeys("pass");
         driver.findElement(By.id("confirmation")).sendKeys("pass");
-        driver.findElement(By.cssSelector("#form-validate > div.buttons-set > button > span > span")).click();
-        driver.close();
+        driver.findElement(By.cssSelector(".button.validation-passed")).click();
     }
 
-
+    @After
+    public void close() {
+        driver.close();
+    }
 }
